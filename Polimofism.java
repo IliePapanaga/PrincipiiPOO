e948088e4db7c4685710ed66b8de52ba134cecba
IliePapanaga <ilie.papanaga@gmail.com>
2023-11-13 20:06:50 +0200

    polimorfism

---
+++ Polimofism.java
@@ -0,0 +1,12 @@
+public class Polimofism {
+    public int addition;
+    public int addition(int a, int b){
+        return a-b;
+    }
+    public double addition(double a, double b){
+        return a-b;
+    }
+    public static void main(String[] args) {
+
+    }
+}
